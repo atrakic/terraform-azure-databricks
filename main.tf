4ee25f96fe948cc5640e5193c6e84a614309e3c2
Admir Trakic <atrakic@users.noreply.github.com>
2023-01-18 17:36:39 +0100

    Add outputs

--- main.tf
+++ main.tf
@@ -30,7 +30,14 @@ output "databricks_host" {
   value = module.workspace.databricks_host
 }
 
-#output "databricks_token" {
-#  value     = module.workspace.databricks_token
-#  sensitive = true
-#}
+output "cluster_id" {
+  value = module.databricks.cluster_id
+}
+
+output "job_url" {
+  value = module.databricks.job_url
+}
+
+output "notebook_url" {
+  value = module.databricks.notebook_url
+}
