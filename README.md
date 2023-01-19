5ab8b8cdec8c9393a06242d484bb6aa210ff5721
Admir Trakic <atrakic@users.noreply.github.com>
2023-01-19 12:49:36 +0100

    Add tf flows

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # azure-databricks-demo-tf
 
-[![terraform](https://github.com/atrakic/azure-databricks-demo-tf/actions/workflows/terraform.yml/badge.svg)](https://github.com/atrakic/azure-databricks-demo-tf/actions/workflows/terraform.yml)
 [![license](https://img.shields.io/github/license/atrakic/azure-databricks-demo-tf.svg)](https://github.com/atrakic/azure-databricks-demo-tf/blob/main/LICENSE)
 
-This repo installs [Azure Databricks](https://learn.microsoft.com/en-us/azure/databricks/).
+This repo provides IaC with Terraform with [Azure Databricks](https://learn.microsoft.com/en-us/azure/databricks/).
 
 Providers used:
 - [azurerm](https://registry.terraform.io/providers/hashicorp/azurerm/latest)
 - [databricks](https://registry.terraform.io/providers/databricks/databricks/latest)
-
-## TODO
-- Deployment pipeline with GH Actions
--- provider.tf
+++ provider.tf
@@ -7,19 +7,32 @@ terraform {
     databricks = {
       source = "databricks/databricks"
     }
+    random = "~> 3.4.3"
   }
+
+  /*
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-xxxx-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "databricks-demo.terraform.tfstate"
+    use_oidc             = true
+  }
+  */
+
   required_version = ">= 1.1.7, < 2.0.0"
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 provider "azurerm" {
+  # OpenID Connect is an authentication method that uses short-lived tokens
+  #use_oidc = true 
   features {
     resource_group {
-      prevent_deletion_if_contains_resources = false
+      prevent_deletion_if_contains_resources = true
     }
   }
 }
 
-# Make client_id, tenant_id, subscription_id and object_id variables
-data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
